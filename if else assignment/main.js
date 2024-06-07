d9160e3dda8d6d0a12d0022bb657d4ee6953518f
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-06 20:01:09 -0700

    Add files via upload

---
+++ if else assignment/main.js
@@ -0,0 +1,19 @@
+var marks = '0<=40';
+if (marks == '80<=100') {
+    console.log("A+ grade bana");
+}
+else if (marks == '70<=79') {
+    console.log("A grade bana");
+}
+else if (marks == '60<=69') {
+    console.log("B grade bana");
+}
+else if (marks == '50<=59') {
+    console.log("C grade bana");
+}
+else if (marks == '40<=49') {
+    console.log("D grade bana");
+}
+else if (marks == '0<=40') {
+    console.log("fail");
+}
